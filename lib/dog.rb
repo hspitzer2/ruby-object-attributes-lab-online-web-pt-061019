b6dc99a0412d0223dded2bbcef75fac22fa1d2ed
Hillary Spitzer <hspitzer2@gmail.com>
2019-06-26 23:27:23 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,10 +1,13 @@
 class Dog
-    def method_name
-
+    def initialize(name)
+      @name = name
     end
 
-    def method_name
-
+    def name
+      @name
     end
 
+    def name=(new_name)
+      @name = new_name
+    end
 end
