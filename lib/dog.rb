cd07d971ec5a46bee81e0e7e7bdddc553455b8a8
Hillary Spitzer <hspitzer2@gmail.com>
2019-06-26 23:47:06 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -10,6 +10,17 @@ class Dog
     def name=(new_name)
       @name = new_name
     end
+    def initialize
+      @breed = breed
+    end
+
+    def name
+      @breed
+    end
+
+    def breed=(new_breed)
+      @breed = new_breed
+    end
 end
 
 fido = Dog.new
